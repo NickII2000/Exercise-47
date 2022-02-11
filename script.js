935276acf613a206d5a0a08e10535429ef33a8df
Nick <Nick_II2000@mail.ru>
2022-02-11 20:11:44 +0600

    Second commit

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
 'use strict';
 
+class Rectangle {
+    constructor(heigth, width) {
+        this.heigth = heigth;
+        this.width = width;
+    }
+
+    calcArea() {
+        return this.heigth * this.width;
+    }
+}
+
+class coloredRectangleWithText extends Rectangle {
+    constructor(heigth, width, text, bgColor) {
+        super(heigth, width);
+        this.text = text;
+        this.bgColor = bgColor;
+    }
+    showMyProps() {
+        console.log(`Текст:  ${this.text}, Цвет: ${this.bgColor}`);
+    }
+}
+
+const r = new Rectangle(5, 10);
+console.log(r);
+console.log(r.calcArea());
+
+const q = new coloredRectangleWithText(2, 2, `Квадрат`, `black`);
+console.log(q);
+console.log(q.calcArea());
+q.showMyProps();
 
 
 
+// КЛАССЫ (ООП):
+// 1. Абстракция - отделяем концепцию (шаблон) от её экземпляров.
+// 2. Наследования - способность объекта (класса) базироваться на другом объекте или классе.
 
 // 1. Обычная функция: this = window, но если 'use strict' - undefined. 
 // 2. Контекст у методов объекта - это сам объект.
